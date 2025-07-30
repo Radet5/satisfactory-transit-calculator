30434823e482d9a2a40b3fc4772f12c38065c7fa
Radet Hannibal-5 <radientradet5@gmail.com>
2025-07-30 19:57:45 -0400

    basic responsive design

--- src/components/recipe-table.tsx
+++ src/components/recipe-table.tsx
@@ -4,6 +4,7 @@ import { useDataContext } from "../context/data-context";
 import { ListTable } from "./list-table";
 import { InOutTable } from "./in-out-table";
 import { calcRatio } from "../modules/helper-functions";
+import { ResponsiveTable } from "./responsive-table";
 
 interface IRecipeTable {
   recipes?: Recipes;
@@ -28,20 +29,38 @@ export const RecipeTable = ({ recipes=[], selectedItemId="", onRowClick }: IReci
     ]);
   }
   
-  const tableBody = recipes.map((recipe, i) => {
+  const rowData = recipes.map((recipe, i) => {
       const curSelectedItemId = selectedItemIds[i];
       const ratio = calcRatio(recipe, items, curSelectedItemId);
 
-      return <tr onClick={(e) => onRowClick(e, recipe)} key={recipe.id}>
-        <td>{recipe.name}</td>
-        <td><InOutTable items={recipe.in} time={recipe.time} selected={curSelectedItemId} onRowClick={(id) => onClickSelectItem(id, i)} /></td>
-        <td><InOutTable items={recipe.out} time={recipe.time} /></td>
-        <td style={{ whiteSpace: "nowrap"}}>1 : {ratio}</td>
-        <td><ListTable itemIds={recipe.producers} /></td>
-      </tr>
+      // return <tr onClick={(e) => onRowClick(e, recipe)} key={recipe.id}>
+      //   <td>{recipe.name}</td>
+      //   <td><InOutTable items={recipe.in} time={recipe.time} selected={curSelectedItemId} onRowClick={(id) => onClickSelectItem(id, i)} /></td>
+      //   <td><InOutTable items={recipe.out} time={recipe.time} /></td>
+      //   <td style={{ whiteSpace: "nowrap"}}>1 : {ratio}</td>
+      //   <td><ListTable itemIds={recipe.producers} /></td>
+      // </tr>
+
+      return {
+        name: recipe.name,
+        in: <InOutTable items={recipe.in} time={recipe.time} selected={curSelectedItemId} onRowClick={(id) => onClickSelectItem(id, i)} />,
+        out: <InOutTable items={recipe.out} time={recipe.time} />,
+        stackRatio: <div style={{ whiteSpace: "nowrap"}}>1 : {ratio}</div>,
+        producers: <ListTable itemIds={recipe.producers} />,
+      };
     }
   );
 
+  return <ResponsiveTable
+            headings={[
+              { id: "name", name: "Name" },
+              { id: "in", name: "In (per m)" },
+              { id: "out", name: "Out (per m)" },
+              { id: "stackRatio", name: "Stack Ratio" },
+              { id: "producers", name: "Producers" },
+            ]}
+            data={rowData}
+          />
   return (
     <table>
       <thead>
---
+++ src/components/responsive-table.tsx
@@ -0,0 +1,78 @@
+import type { ReactNode } from "react";
+import type { BaseType } from "../types";
+import "./responsive-table.css";
+
+type TableHeadingData = BaseType;
+type TableRowData = { [key: TableHeadingData["id"]]: ReactNode };
+
+interface IResponsiveTable {
+  headings: Array<TableHeadingData>;
+  data: Array<TableRowData>;
+}
+
+export const ResponsiveTable = ({ headings, data }: IResponsiveTable) => {
+
+  const ths = headings.map(heading => <TableHeading heading={heading.name} key={heading.id}/>);
+  const rows = data.map(rowData => <TableRow rowData={rowData} headings={headings} />);
+
+  return <div className="table-container">
+  <table className="responsive-table">
+    <caption>Caption</caption>
+    <thead>
+      <tr>
+        {ths}
+      </tr>
+    </thead>
+    <tbody>
+      {rows}
+    </tbody>
+  </table>
+</div>
+}
+
+interface ITableHeading {
+  heading: string;
+}
+
+const TableHeading = ({ heading }: ITableHeading) => {
+  return <th scope="col">{heading}</th>
+}
+
+interface ITableRow {
+  headings: Array<TableHeadingData>;
+  rowData: TableRowData;
+}
+
+const TableRow = ({ rowData, headings }: ITableRow) =>
+  <tr>
+    {headings.map((heading, i) =>
+      <TableCell
+        index={i}
+        heading={heading.name}
+        content={rowData[heading.id]}
+      />
+    )}
+  </tr>
+
+interface ITableCell {
+  index: number
+  heading: string,
+  content: ReactNode,
+}
+
+const TableCell = ({ index, heading, content }: ITableCell) => {
+  if (index > 0) return (
+      <td
+        data-title={heading}
+      >
+        {content}
+      </td>
+  );
+  return (
+    <th scope="row">
+      {content}
+    </th>
+  );
+};
+
+
